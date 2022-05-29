773a25b3d66b82d5849ef6a7b7a0be1a641eb6ee
KenChiu429 <102272206+KenChiu429@users.noreply.github.com>
2022-05-29 21:07:55 +1000

    Add files via upload

---
+++ update_lambda.py
@@ -0,0 +1,25 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamoDb = boto3.resource('dynamodb')
+table = dynamoDb.Table('Image')
+
+
+def lambda_handler(event, context):
+    keyCondition = Key('url').eq(event['url'])
+    response = table.query(
+        KeyConditionExpression=keyCondition,
+    )
+    tags_set = set(response['Items'][0]['tags'])
+    if(event['type'] == 1):
+        print(event['tags'])
+        item = {
+            "url": event['url'],
+            "tags": list(tags_set.union(set(event['tags'])))
+        }
+        res = table.put_item(Item=item)
+        print(res)
+    elif(event['type'] == 2):
+        item = response['Items'][0]
+        item['tags'] = list(tags_set.difference(set(event['tags'])))
+        table.put_item(Item=item)
